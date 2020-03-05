92f25861e8795a8d67a926103f5d3adc27b24def
Gleb Levchik <lvchkgb@gmail.com>
2020-03-06 00:13:54 +0300

    init commit

---
+++ table.py
@@ -0,0 +1,15 @@
+value = input("Введите значение: ")
+if value:
+    ximia = int(value)
+    if ximia == 3:
+        print("Li")
+    elif ximia == 25:
+        print("Mn")
+    elif ximia == 80:
+        print("Hg")
+    elif ximia == 17:
+        print("Cl")
+    else:
+        print("Что это?!")
+else:
+    print("Введите значение !")
